a22e1baf59d87cd70f1b29fdd3d5d41e0ff6ee31
Kabasele <pepexykabasele@gmail.com>
2021-07-28 16:58:12 +0200

    exercice for the multication table

---
+++ mulliplication.py
@@ -0,0 +1,4 @@
+nomb = input()
+for i in range(1,13):
+
+	print(nomb,"x",i,"=",nomb*i)
---
+++ tabmltit.py
@@ -0,0 +1,7 @@
+for x in range(1,13):
+
+	print("Table de multication par :", x)
+
+	for z in range(1,13):
+		
+		print(x, "x", z,"=",x*z)
