6bc65ce21acaee3737c337aa3ec4da060f5befbe
Kabasele <pepexykabasele@gmail.com>
2021-08-16 17:56:10 +0200

    update list comprhension

--- list_comprhension.py
+++ list_comprhension.py
@@ -7,12 +7,17 @@ ma_liste_de_nombres = [1, 2, 3, 4, 5]
 print([ n + 3 for n in ma_liste_de_nombres ])
 
 '''
-Si je veux la somme des carrés des entiers de 1 à 10, je peux par exemple faire :
+## Si je veux la somme des carrés des entiers de 1 à 10, je peux par exemple faire :
 
 print(sum( [ n**2 for n in range(1,11) ] ))
 
-Si je veux les longueurs des mots d'une liste de mots :
+##  je veux les longueurs des mots d'une liste de mots :
 
 ma_liste_de_mots = [ "Cosinus", "Sinus", "Tangente", "Cotangente" ]
 print([ len(mot) for mot in ma_liste_de_mots])
+
+## Si je veux récupérer la première lettre des mots d'une liste commençant par une voyelle :
+
+ma_liste_de_mots= ["maths", "info", "python", "exposant", "alpha", "fonction", "parabole", "equilateral", "orthogonal", "cercle", "isocèle" ]
+print([ mot[0] for mot in ma_liste_de_mots if mot[0] in "aeiouy"])
 '''
