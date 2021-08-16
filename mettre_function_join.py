8d00ee8de378de4dd1771f6af1dc7c2d548a443c
Kabasele <pepexykabasele@gmail.com>
2021-08-16 17:45:31 +0200

    add the function separator

---
+++ mettre_function_join.py
@@ -0,0 +1,9 @@
+'''
+separateur.join(liste) : L'exact contraire de split(). 
+Crée un texte à partir d'une liste de mots en intercalant entre chaque mot le separateur.
+'''
+
+
+print(" / ".join( ["Janvier", "Février", "Mars", "Avril" ]))
+print(" <=> ".join( [ "x² - 1 = 0", "x² = 1", "x = 1 ou x = -1"]))
+print("".join([ "3", ".", "1", "4", "1", "5", "9" ]))
