82acf45ed16bf12bef2cabf56dec82fba0fdda54
Kabasele <pepexykabasele@gmail.com>
2021-08-17 14:23:21 +0200

    add variable local and global

---
+++ variables_locales_et_globales.py
@@ -0,0 +1,7 @@
+'''
+Les variables locales et globales.
+
+La chose importante à comprendre sur les variables est : 
+Toutes les variables que l'on crée et utilise à l'intérieur d'une fonction n'existent qu'à l'intérieur de cette fonction.
+Par exemple si je lance ceci :
+'''
