5309e685b83bf423b12ce800b106f2909e8868fa
Kabasele <pepexykabasele@gmail.com>
2021-08-17 14:01:17 +0200

    add list for iter

---
+++ list_de_list.py
@@ -0,0 +1,13 @@
+'''
+Listes de listes :
+Dans une liste, on peut mettre beaucoup de choses différentes et entre autre 
+des listes ce qui est bien pratique si on veut stocker comme dans un tableau. 
+Par exemple une grille de morpion pourrait s’écrire : grille = [["O", "X", "O"], ["O", "X", "X"], ["X", "O", "O"]] 
+Pour récupérer l'élément de la deuxième ligne troisième colonne il suffit que j'écrive grille[1][2] car les listes commencent à 0 donc il faut penser à décaler d'un cran et de plus, grille[1] est la liste ["O", "X", "X"], c'est donc naturel que pour récupéré le troisième élément, on écrive grille[1][2]. Les listes de listes servent souvent à représenter un tableau mais on n'est pas obligé de s’arrêter 
+à une profondeur de deux. On peut faire des listes de listes de listes...
+'''
+
+liste = [ "T", "e", "x", "t", "e" ]
+for i, c in enumerate(liste):
+    print(i,c)
+
