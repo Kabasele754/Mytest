3c7b2c64e81313f7e08297dbed0f58c331bece80
Kabasele <pepexykabasele@gmail.com>
2021-08-17 14:24:08 +0200

    add variable local and global exemple

--- variables_locales_et_globales.py
+++ variables_locales_et_globales.py
@@ -5,3 +5,8 @@ La chose importante à comprendre sur les variables est :
 Toutes les variables que l'on crée et utilise à l'intérieur d'une fonction n'existent qu'à l'intérieur de cette fonction.
 Par exemple si je lance ceci :
 '''
+
+def fonction(n):
+    a=n+1
+fonction(3)
+print(a)
