71b4c3dbd6627e3cb652bf5a96744581097590fb
Kabasele <pepexykabasele@gmail.com>
2021-08-18 19:33:41 +0200

    add variable locale et globale

--- variables_locales_et_globales.py
+++ variables_locales_et_globales.py
@@ -5,8 +5,13 @@ La chose importante à comprendre sur les variables est :
 Toutes les variables que l'on crée et utilise à l'intérieur d'une fonction n'existent qu'à l'intérieur de cette fonction.
 Par exemple si je lance ceci :
 '''
-
+a=18
 def fonction(n):
     a=n+1
 fonction(3)
 print(a)
+
+'''
+J'obtiendrais que a n'existe pas. Le a à l'intérieur de la fonction est ce qu'on appelle une variable locale qui n'existe que dans la fonction pas en dehors.
+Encore pire :
+'''
