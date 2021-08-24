a3bec5f2e94410d09cb6976f6abfa03b3ca98f9b
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-08-24 21:17:25 +0200

    Update Swap objet.py

--- Swap objet.py
+++ Swap objet.py
@@ -7,3 +7,10 @@ left, right = right, left
 '''
 Par exemple :
 a,b = a,b
+
+'''
+Alors, comment ça marche? 1. Tout d'abord, le membre de droite b,a est évalué : 
+cela signifie qu'une variable de deux éléments est créé en mémoire. Les deux éléments sont les objets désignés 
+par les identifiants b et a, qui existaient avant que l'instruction ne soit rencontrée. Après la création de ce tuple, aucune affectation de 
+cet objet variable n'a encore été effectuée.
+'''
