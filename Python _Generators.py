0eb23b234d3336b56e86f96fdae2ec5391e7a4db
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-08-29 17:04:11 +0200

    Update Python _Generators.py

--- Python _Generators.py
+++ Python _Generators.py
@@ -1,1 +1,10 @@
+# Générateurs Python
+'''Les générateurs Python sont très puissants pour gérer les opérations qui nécessitent une grande quantité de mémoire.
+Commençons par un exemple simple. La fonction ci-dessous imprime une séquence infinie de nombres.'''
+
+def  generator_example1 (): 
+    count  =  0 
+    while  True : 
+        return  count 
+        count += 1
 
