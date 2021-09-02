711585ffb0d64c7592f7377bd19f6d568bc15baf
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-09-02 18:06:04 +0200

    Update list_comprhension.py

--- list_comprhension.py
+++ list_comprhension.py
@@ -1,12 +1,12 @@
-'''
+"""
 Une des forces de Python : la possibilité de créer une liste à partir d'autres en rajoutant des conditions à la volée et le tout en une ligne !
 Donnons des exemples : Pour augmenter de 3 tous les éléments d'une liste de nombres
-'''
+"""
 
 ma_liste_de_nombres = [1, 2, 3, 4, 5]
 print([ n + 3 for n in ma_liste_de_nombres ])
 
-'''
+"""
 ## Si je veux la somme des carrés des entiers de 1 à 10, je peux par exemple faire :
 
 print(sum( [ n**2 for n in range(1,11) ] ))
@@ -20,4 +20,4 @@ print([ len(mot) for mot in ma_liste_de_mots])
 
 ma_liste_de_mots= ["maths", "info", "python", "exposant", "alpha", "fonction", "parabole", "equilateral", "orthogonal", "cercle", "isocèle" ]
 print([ mot[0] for mot in ma_liste_de_mots if mot[0] in "aeiouy"])
-'''
+"""
