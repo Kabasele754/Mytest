3305fd69b41709fb3cc4e12a094ea20100e246c3
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-09-03 06:52:46 +0200

    Update compteur_accumilateur.py

--- compteur_accumilateur.py
+++ compteur_accumilateur.py
@@ -1,1 +1,11 @@
 
+def compteur_accumulateur(nbr:int):
+    compteur = 0
+    accumulateur = 0
+    
+    while compteur < nbr:
+       compteur = compteur +1
+       accumulateur = accumulateur + compteur
+    
+
+   return accumulateur
