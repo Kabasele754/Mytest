ffb52171b7e406ff7de038346aa2a83465495d57
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-09-03 06:57:48 +0200

    Update compteur_accumilateur.py

--- compteur_accumilateur.py
+++ compteur_accumilateur.py
@@ -5,6 +5,7 @@ def compteur_accumulateur(nbr:int):
     accumulateur = 0
     
     while compteur < nbr:
+       # tant que le compteur est inférieure au nombre passé dans le paramètre de la fonction 
        compteur = compteur +1
        accumulateur = accumulateur + compteur
     
