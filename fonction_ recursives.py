e8ef1bbf4e425feb84d81dcd846f2b4535bcc919
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-09-04 21:47:33 +0200

    Update fonction_ recursives.py

--- fonction_ recursives.py
+++ fonction_ recursives.py
@@ -1,10 +1,10 @@
-'''
+"""
 Une fonction récursive est tous simplement une fonction qui s'appelle elle même.
 Prenons un exemple : Le calcul de la somme des entiers entre 1 et n.
 L'idée est d'expliquer comment il faut commencer et ce qu'il faut faire pour passer de l'étape n-1 à l'étape n. Python s'occupe du reste.
 Pour commencer, si n vaut 1, la somme vaut 1. Ensuite si j'ai déjà calculé la somme de 1 à n-1, il suffit de lui ajouter n pour obtenir 
 la somme de 1 à n. Cela donnerait le programme suivant :
-'''
+"""
 
 def somme(n):
     if n==1:
