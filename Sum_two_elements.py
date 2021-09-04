fbf6dd682ccdb18d3b85d382a0b10b7477a86e06
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-09-04 21:40:56 +0200

    Update Sum_two_elements.py

--- Sum_two_elements.py
+++ Sum_two_elements.py
@@ -1,9 +1,10 @@
 import functools as fts
 
-'''
+"""
 Pour la somme de deux nombres
 Cette fonction permet de faire la somme de plus élément passer dans une liste
-'''
+"""
+
 def sum_two_elements(a,b):
     return a+b
 number = [1,5,7,9,3,2]
