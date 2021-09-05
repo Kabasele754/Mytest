0a7dce47b507f305347cc8093b42d041f9f5d4e5
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-09-05 11:28:30 +0200

    Update Swap objet.py

--- Swap objet.py
+++ Swap objet.py
@@ -1,18 +1,18 @@
-'''
+"""
 En Python, deux valeurs de variables 
 échangées à l'aide de cette syntaxe:
 
 left, right = right, left
 
-'''
+"""
 Par exemple :
 a,b = a,b
 
-'''
+"""
 Alors, comment ça marche? 
 
 1. Tout d'abord, le membre de droite b,a est évalué : 
 cela signifie qu'une variable de deux éléments est créé en mémoire. Les deux éléments sont les objets désignés 
 par les identifiants b et a, qui existaient avant que l'instruction ne soit rencontrée. Après la création de ce tuple, aucune affectation de 
 cet objet variable n'a encore été effectuée.
-'''
+"""
