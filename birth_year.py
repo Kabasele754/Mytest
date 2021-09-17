c12d97ea7fdd1e3b0a601767356fb062e29849c3
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-09-17 12:08:19 +0200

    Update birth_year.py

--- birth_year.py
+++ birth_year.py
@@ -1,1 +1,13 @@
 
+birth_year : int = int(input("Bonjour vous pouvez donner votre annee de naissance : "))
+
+age : int = 2021 - birth_year
+
+if age > 18:
+    print("Merci pour votre age tu es Mageur avec ", age, "ans")
+
+elif age < 18:
+    print("Merci pour votre age tu es Mineur avec ", age, "ans")
+    
+else:
+   print("Merci pour votre age tu es Adolescent avec ", age, "ans")
