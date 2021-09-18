f3436657166c164f1d22ce6a0288e9251f087559
Achille kabasele <67704765+Kabasele754@users.noreply.github.com>
2021-09-18 20:25:13 +0200

    Update exempl_bool.py

--- exempl_bool.py
+++ exempl_bool.py
@@ -1,1 +1,8 @@
-
+nom = input("Quel est votre nom? ")
+mdp = input("Quel est le mot de passe? ")
+if nom == "Kaoutar" and mdp == "One":
+print("Bienvenue Kaoutar")
+elif nom == "Hiba" and password == "Two":
+print("Welcome Hiba")
+else:
+print("Utilisateur non reconnu")
